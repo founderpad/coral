d836eb87b778cf6b7da8815728b4c550fdea3963
Jamie <jamie@Jamies-MacBook-Air.local>
2021-10-02 23:37:57 +0100

    Update to include tests for LoggedOut page.

---
+++ src/__test__/LoggedOut.test.tsx
@@ -0,0 +1,22 @@
+import LoggedOut from 'pages/loggedout';
+import { Provider } from 'react-redux';
+import store from 'utils/store';
+import { render } from '__test__/testUtils';
+
+const setup = () => render(
+	<Provider store={store}>
+		<LoggedOut />
+	</Provider>
+);
+
+describe('Logged out page', () => {
+	it('matches snapshot', () => {
+		const { asFragment } = setup();
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+    it('should render LoggedOut page', () => {
+		const { getByRole } = setup();
+		expect(getByRole('link')).toBeInTheDocument();
+    });
+});
