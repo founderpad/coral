38e11ffe3413044213ed0df46473c5c2a4e4ba77
Jamie <jamie@Jamies-MacBook-Air.local>
2021-10-08 16:05:28 +0100

    Update

--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -1,12 +1,11 @@
-export { SubmitButton } from './SubmitButton';
 export { BaseButton } from './BaseButton';
 export { CancelButton } from './CancelButton';
-export { PrimaryButton } from './PrimaryButton';
-export { GoBackButton } from './GoBackButton';
 export {
 	DeleteButton,
-	DeleteMenuButton,
-	DeleteIconButton
+	DeleteIconButton,
+	DeleteMenuButton
 } from './DeleteButton';
-
+export { GoBackButton } from './GoBackButton';
 export { EditButton } from './iconbuttons/EditButton';
+export { PrimaryButton } from './PrimaryButton';
+export { SubmitButton } from './SubmitButton';
