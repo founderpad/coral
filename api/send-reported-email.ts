4af950afb4351ba028aaccec9bcba8ea51271112
Jamie <jamie@Jamies-MacBook-Air.local>
2021-11-06 19:05:00 +0000

    Adding aws-sdk to see if this is failing the build

--- api/send-reported-email.ts
+++ api/send-reported-email.ts
@@ -1,5 +1,13 @@
+const AWS = require('aws-sdk');
 
 export default (_req, res): any => {
+
+    const CONFIG = {
+        apiVersion: '2010-12-01',
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
+    };
 
     console.log('CUSTOM API')
 	res.send('OKAY1')
