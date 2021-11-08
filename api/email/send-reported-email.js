647c4cdea4ecb13d2fbf57aa2075a02178c74fbb
Jamie <jamie@Jamies-MacBook-Air.local>
2021-11-08 14:53:27 +0000

    Updated

--- api/email/send-reported-email.js
+++ api/email/send-reported-email.js
@@ -1,24 +1,14 @@
 const AWS = require('aws-sdk');
 
-// const CONFIG = {
-//     apiVersion: '2010-12-01',
-//     accessKeyId: 'AKIAXAVHTNCXREX2LSFD',
-//     secretAccessKey: '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',
-//     region: 'eu-west-1'
-// };
-
 const CONFIG = {
 	apiVersion: '2010-12-01',
 	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-	region: 'eu-west-1'
+	region: process.env.AWS_DEFAULT_REGION
 };
 
-// const CONFIG = AWS.config.lo
 const AWS_SES = new AWS.SES(CONFIG);
-// const AWS_SES = new AWS.SES({ apiVersion: '2010-12-01' });
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async (req, res) => {
 	await AWS_SES.sendEmail({
 		Source: 'jamie@founderpad.com',
