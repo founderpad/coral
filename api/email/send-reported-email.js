ca8f8ab99af1b5117023fcba9e37729000384a4e
Jamie <jamie@Jamies-MacBook-Air.local>
2021-11-09 12:07:53 +0000

    Updated

--- api/email/send-reported-email.js
+++ api/email/send-reported-email.js
@@ -10,45 +10,8 @@ const CONFIG = {
 const AWS_SES = new AWS.SES(CONFIG);
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default (req, res) => {
-	// await AWS_SES.sendEmail({
-	// 	Source: 'jamie@founderpad.com',
-	// 	Destination: {
-	// 		ToAddresses: [`${req.body.event.data.new.recipientEmail}`],
-	// 		BccAddresses: ['jamie@founderpad.com']
-	// 		// ToAddresses: ['success@simulator.amazonses.com'],
-	// 	},
-	// 	Message: {
-	// 		Body: {
-	// 			Html: {
-	// 				Charset: 'UTF-8',
-	// 				Data: `<html>
-	//                             <head></head>
-	//                             <body>
-	//                                 <p>Hi ${
-	// 									req.body.event.data.new.recipientName
-	// 								},</p>
-	//                                 <p>Your ${req.body.event.data.new.type.toLowerCase()} <i>"${
-	// 					req.body.event.data.new.content
-	// 				}"</i> has been reported for the following reason:
-	//                                     <strong>${
-	// 										req.body.event.data.new.reason
-	// 									}</strong>
-	//                                 </p>
-	//                                 <p>We will carefully monitor this and, if necessary, will remove this ${req.body.event.data.new.type.toLowerCase()}.</p><br/>
-	//                                 <strong>The founderpad team</strong>
-	//                             </body>
-	//                         </html>`
-	// 			}
-	// 		},
-	// 		Subject: {
-	// 			Charset: 'UTF-8',
-	// 			Data: `Your ${req.body.event.data.new.type.toLowerCase()} has been reported`
-	// 		}
-	// 	}
-	// }).promise();
-
-	AWS_SES.sendEmail({
+export default async (req, res) => {
+	await AWS_SES.sendEmail({
 		Source: 'jamie@founderpad.com',
 		Destination: {
 			// ToAddresses: [`${req.body.event.data.new.recipientEmail}`],
@@ -56,18 +19,55 @@ export default (req, res) => {
 			BccAddresses: ['jamie@founderpad.com']
 		},
 		Message: {
-			Subject: {
-				Charset: 'UTF-8',
-				Data: `Your ${req.body.event.data.new.type.toLowerCase()} has been reported`
-			},
 			Body: {
 				Html: {
 					Charset: 'UTF-8',
-					Data: 'Test email'
+					Data: `<html>
+	                            <head></head>
+	                            <body>
+	                                <p>Hi ${
+										req.body.event.data.new.recipientName
+									},</p>
+	                                <p>Your ${req.body.event.data.new.type.toLowerCase()} <i>"${
+						req.body.event.data.new.content
+					}"</i> has been reported for the following reason:
+	                                    <strong>${
+											req.body.event.data.new.reason
+										}</strong>
+	                                </p>
+	                                <p>We will carefully monitor this and, if necessary, will remove this ${req.body.event.data.new.type.toLowerCase()}.</p><br/>
+	                                <strong>The founderpad team</strong>
+	                            </body>
+	                        </html>`
 				}
+			},
+			Subject: {
+				Charset: 'UTF-8',
+				Data: `Your ${req.body.event.data.new.type.toLowerCase()} has been reported`
 			}
 		}
-	});
+	}).promise();
+
+	// await AWS_SES.sendEmail({
+	// 	Source: 'jamie@founderpad.com',
+	// 	Destination: {
+	// 		// ToAddresses: [`${req.body.event.data.new.recipientEmail}`],
+	// 		ToAddresses: ['success@simulator.amazonses.com'],
+	// 		BccAddresses: ['jamie@founderpad.com']
+	// 	},
+	// 	Message: {
+	// 		Subject: {
+	// 			Charset: 'UTF-8',
+	// 			Data: `Your ${req.body.event.data.new.type.toLowerCase()} has been reported`
+	// 		},
+	// 		Body: {
+	// 			Html: {
+	// 				Charset: 'UTF-8',
+	// 				Data: 'Test email'
+	// 			}
+	// 		}
+	// 	}
+	// }).promise();
 
 	res.send('Email sent successfully');
 };
