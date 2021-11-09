30632ffe75e24c8496afa3c2565b795c81a1c58b
Jamie <jamie@Jamies-MacBook-Air.local>
2021-11-09 16:20:48 +0000

    Updated

--- api/index.js
+++
@@ -1,4 +0,0 @@
-// require('dotenv').config();
-import dotevn from 'dotenv';
-dotevn.config();
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSw4QkFBOEI7QUFDOUIsT0FBTyxNQUFNLE1BQU0sUUFBUSxDQUFDO0FBQzVCLE1BQU0sQ0FBQyxNQUFNLEVBQUUsQ0FBQyJ9
