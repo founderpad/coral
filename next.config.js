16dbdcc6fd9cc92ffdc04a95d6a703b040822741
Jamie <jamie@Jamies-MacBook-Air.local>
2021-11-13 21:51:49 +0000

    Updated

--- next.config.js
+++ next.config.js
@@ -4,17 +4,16 @@ module.exports = {
 			beforeFiles: [
 				// if the host is `app.acme.com`,
 				// this rewrite will be applied
-				{
-					source: '/:path*',
-					has: [
-						{
-							type: 'host',
-							value: 'app.founderpad.com'
-						}
-					],
-					destination: '/app/:path*'
-				}
-
+				// {
+				// 	source: '/:path*',
+				// 	has: [
+				// 		{
+				// 			type: 'host',
+				// 			value: 'app.founderpad.com'
+				// 		}
+				// 	],
+				// 	destination: '/app/:path*'
+				// }
 				// {
 				// 	source: '/',
 				// 	destination: 'https://founderpad-24c952.webflow.io/'
