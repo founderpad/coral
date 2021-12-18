d0ace67cc648034e5714d10bec31c8032e092cc6
Jamie <jamie@Jamies-MacBook-Air.local>
2021-12-18 15:40:53 +0000

    Updated to use tables. Old school HTML for the win it seems, until email clients decide to support the latest standards.

--- api/email/user/send-welcome-email.js
+++ api/email/user/send-welcome-email.js
@@ -45,8 +45,15 @@ export default (req, res) => {
 										display: flex;
 										justify-content: center;
 									}
-									table {
-										background-color: #F7FAFC;
+									#outer-container {
+										max-width: 700px;
+										display: flex;
+										flex: 1;
+										margin: 32px auto;
+										background-color: white;
+									}
+									#inner-container {
+										background-color: #F7FAFC;;
 										display: flex;
 										flex: 1;
 										margin: 32px auto;
@@ -63,7 +70,9 @@ export default (req, res) => {
 									th {
 										display: flex;
 										justify-content: center;
-										background-color: white;
+									}
+									th > img {
+										margin: 0 auto;
 									}
 									h2, h3 {
 										margin-bottom: 0;
@@ -71,64 +80,70 @@ export default (req, res) => {
 								</style>
 							</head>
 							<body>
-								<span id="founderpadlogo">
-									<img src="https://backend-19728797.nhost.app/storage/o/public/assets/founderpad-logo200x41.png" />
-								</span>
-								<table>
+								<table id="outer-container">
+									<th>
+										<img src="https://backend-19728797.nhost.app/storage/o/public/assets/founderpad-logo200x41.png" />
+									</th>
 									<tr>
-										<td><h2>Hi ${req.body.event.data.new.display_name}, welcome to founderpad! 🎉 </h2></td>
-										<tr>
-											<td>
-												We are really excited to have you on board, and hope you are just as excited as us at the prospect of
-												getting feedback on your business ideas, innovating, and launching to the world. 🚀
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<h3>
-													Getting started
-												</h3>
-												<p>
-													Click <a href="http://www.founderpad.com/login" target="_blank" style="text-decoration: none;">here</a>
-													to login and get started immediately. Network, post ideas and collaborate!
-												</p>
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<h3>
-													FAQs
-												</h3>
-												<p>
-													If you have any questions, they may already be covered inside our FAQs 
-													<a href="https://www.founderpad.com/faqs" target="_blank" style="text-decoration: none;">here</a>.<br/><br/>
-													Can't find an answer to what you're looking for? No problem!
-													We're always keen to hear feedback on the platform and how we can improve it.<br/><br/>
-													Feel free to email us here at 
-													<a href="mailto:countact@founderpad.com" target="_blank" style="text-decoration: none;">contact@founderpad.com</a> 
-													if you have any questions about the platform.
-												</p>
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<h3>
-													Thanks for joining
-												</h3>
-												<p>
-													We hope you enjoy what we have to offer, and look forward to the extensive updates we have 
-													coming in the future.
-												</p>
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<strong>The founderpad team</strong>
-											</td>
-										</tr>
-									</tr>
-								</table>
-							</body>
+										<td>
+											<table id="inner-container">
+												<tr>
+													<td><h2>Hi ${req.body.event.data.new.display_name}, welcome to founderpad! 🎉 </h2></td>
+													<tr>
+														<td>
+															We are really excited to have you on board, and hope you are just as excited as us at the prospect of
+															getting feedback on your business ideas, innovating, and launching to the world. 🚀
+														</td>
+													</tr>
+													<tr>
+														<td>
+															<h3>
+																Getting started
+															</h3>
+															<p>
+																Click <a href="http://www.founderpad.com/login" target="_blank" style="text-decoration: none;">here</a>
+																to login and get started immediately. Network, post ideas and collaborate!
+															</p>
+														</td>
+													</tr>
+													<tr>
+														<td>
+															<h3>
+																FAQs
+															</h3>
+															<p>
+																If you have any questions, they may already be covered inside our FAQs 
+																<a href="https://www.founderpad.com/faqs" target="_blank" style="text-decoration: none;">here</a>.<br/><br/>
+																Can't find an answer to what you're looking for? No problem!
+																We're always keen to hear feedback on the platform and how we can improve it.<br/><br/>
+																Feel free to email us here at 
+																<a href="mailto:countact@founderpad.com" target="_blank" style="text-decoration: none;">contact@founderpad.com</a> 
+																if you have any questions about the platform.
+															</p>
+														</td>
+													</tr>
+													<tr>
+														<td>
+															<h3>
+																Thanks for joining
+															</h3>
+															<p>
+																We hope you enjoy what we have to offer, and look forward to the extensive updates we have 
+																coming in the future.
+															</p>
+														</td>
+													</tr>
+													<tr>
+														<td>
+															<strong>The founderpad team</strong>
+														</td>
+													</tr>
+												</tr>
+											</table>
+										</td>
+									</tr> 
+            					</table>
+        					</body>
 						</html>
 					`
 				}
