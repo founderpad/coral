0f791db1177a2f1e980f530ce27261a1e9b6449e
Jamie <jamie@Jamies-MacBook-Air.local>
2022-01-01 12:53:53 +0000

    Updated

--- src/pages/ideas/components/CreateEditIdeaForm.tsx
+++ src/pages/ideas/components/CreateEditIdeaForm.tsx
@@ -4,7 +4,6 @@ import { InputField } from 'components/input/InputField';
 import { SelectField } from 'components/input/SelectField';
 import { SwitchField } from 'components/input/SwitchField';
 import { TextareaField } from 'components/input/TextareaField';
-import { StackLayout } from 'components/layouts';
 import AppDivider from 'components/shared/AppDivider';
 import {
 	TCreateIdeaMutation,
@@ -61,97 +60,99 @@ const CreateEditIdeaForm = ({ idea }: { idea?: TIdeas }): JSX.Element => {
 			name={'create-edit-Create edit idea form-form'}
 			onSubmit={handleSubmit(createIdeaMutation)}
 			noValidate
+			stackProps={{ spacing: 10 }}
 		>
 			{' '}
-			<StackLayout spacing={10}>
-				<InputField
-					id="name"
-					name="name"
-					label="Name of your idea"
-					placeholder="Name of your idea (max. 100 characters)"
-					error={errors['name']}
-					errorText="Please enter a name for your idea"
-					control={control}
-					rules={{ maxLength: 100 }}
-					size={'lg'}
-					fontSize={'lg'}
-					isRequired
-				/>
-
-				<AppDivider />
-
-				<TextareaField
-					id="description"
-					name="description"
-					label="Description of your idea"
-					placeholder="Write a description about your idea (max. 500 characters)"
-					error={errors['description']}
-					errorText="Please enter your idea description (max. 500 characters)"
-					control={control}
-					rules={{ maxLength: 500 }}
-					isRequired
-				/>
-
-				<SelectField
-					id="field"
-					name="field"
-					label="What field is your idea?"
-					error={errors['field']}
-					errorText="Please select the field for your idea."
-					placeholder="field"
-					size={'md'}
-					options={industriesList()}
-					control={control}
-					isRequired
-				/>
-
-				<SelectField
-					id="status"
-					name="status"
-					label="What is its current status?"
-					error={errors['status']}
-					errorText="Please select the status for your idea."
-					placeholder="status"
-					size={'md'}
-					options={ideasStatusList()}
-					control={control}
-					isRequired
-				/>
-
-				<TextareaField
-					id="competitors"
-					name="competitors"
-					label="Your competitors"
-					placeholder="List your competitors (max. 200 characters)"
-					control={control}
-					error={errors['competitors']}
-					errorText="Competitors must not be more than 200 characters"
-					rules={{ maxLength: 200 }}
-				/>
-
-				<TextareaField
-					id="team"
-					label="Your team"
-					name="team"
-					placeholder="List your team (max. 200 characters)"
-					control={control}
-					error={errors['team']}
-					errorText="Team must not be more than 200 characters"
-					rules={{ maxLength: 200 }}
-				/>
-
-				<TextareaField
-					id="additional_information"
-					label="Additional information"
-					name="additional_information"
-					placeholder="Any additional information"
-					control={control}
-					error={errors['additional_information']}
-					errorText="Additional information must not be more than 200 characters"
-					rules={{ maxLength: 200 }}
-				/>
-
-				{/* <FormControl>
+			{/* <StackLayout spacing={10}> */}
+			<InputField
+				id="name"
+				name="name"
+				label="Name of your idea"
+				placeholder="Name of your idea (max. 100 characters)"
+				error={errors['name']}
+				errorText={
+					errors['name']?.type === 'required'
+						? 'Please enter a name for your idea'
+						: 'Idea name can not be more than 100 characters'
+				}
+				control={control}
+				rules={{ required: true, maxLength: 100 }}
+				size={'lg'}
+				fontSize={'lg'}
+				isRequired
+			/>
+			<AppDivider />
+			<TextareaField
+				id="description"
+				name="description"
+				label="Description of your idea"
+				placeholder="Write a description about your idea (max. 500 characters)"
+				error={errors['description']}
+				errorText={
+					errors['description']?.type === 'required'
+						? 'Please enter a description for your idea'
+						: 'Description can not be more than 500 characters'
+				}
+				maxRows={5}
+				control={control}
+				rules={{ required: true, maxLength: 500 }}
+				isRequired
+			/>
+			<SelectField
+				id="field"
+				name="field"
+				label="What field is your idea?"
+				error={errors['field']}
+				errorText="Please select the field for your idea"
+				placeholder="field"
+				size={'md'}
+				options={industriesList()}
+				control={control}
+				isRequired
+			/>
+			<SelectField
+				id="status"
+				name="status"
+				label="What is its current status?"
+				error={errors['status']}
+				errorText="Please select the status for your idea"
+				placeholder="status"
+				size={'md'}
+				options={ideasStatusList()}
+				control={control}
+				isRequired
+			/>
+			<TextareaField
+				id="competitors"
+				name="competitors"
+				label="Your competitors"
+				placeholder="List your competitors (max. 200 characters)"
+				control={control}
+				error={errors['competitors']}
+				errorText="Competitors must not be more than 200 characters"
+				rules={{ maxLength: 200 }}
+			/>
+			<TextareaField
+				id="team"
+				label="Your team"
+				name="team"
+				placeholder="List your team (max. 200 characters)"
+				control={control}
+				error={errors['team']}
+				errorText="Team must not be more than 200 characters"
+				rules={{ maxLength: 200 }}
+			/>
+			<TextareaField
+				id="additional_information"
+				label="Additional information"
+				name="additional_information"
+				placeholder="Any additional information"
+				control={control}
+				error={errors['additional_information']}
+				errorText="Additional information must not be more than 200 characters"
+				rules={{ maxLength: 200 }}
+			/>
+			{/* <FormControl>
 					<Heading as={'h5'} size={'sm'} color={'fpGrey.900'}>
 						Supporting material
 					</Heading>
@@ -178,27 +179,25 @@ const CreateEditIdeaForm = ({ idea }: { idea?: TIdeas }): JSX.Element => {
 					<FileUploader label="Pitch Deck" />
 					<FileUploader label="Other" />
 				</Stack> */}
-
-				<SwitchField
-					id="is_published"
-					name="is_published"
-					label="Publish your idea"
-					helperText="You can change this after it's been created"
-					defaultChecked={true}
-					control={control}
-				/>
-
-				<SubmitButton
-					name={'create-idea-button'}
-					label={(idea ? 'Update your ' : 'Create your ') + 'idea'}
-					alignSelf={'center'}
-					isLoading={isSubmitting}
-					disabled={!isValid || isSubmitting}
-					mt={'auto'}
-					w={{ base: 'full', sm: 'xs' }}
-					size={'md'}
-				/>
-			</StackLayout>
+			<SwitchField
+				id="is_published"
+				name="is_published"
+				label="Publish your idea"
+				helperText="You can change this after it's been created"
+				defaultChecked={true}
+				control={control}
+			/>
+			<SubmitButton
+				name={'create-idea-button'}
+				label={(idea ? 'Update your ' : 'Create your ') + 'idea'}
+				alignSelf={'center'}
+				isLoading={isSubmitting}
+				disabled={!isValid || isSubmitting}
+				mt={'auto'}
+				w={{ base: 'full', sm: 'xs' }}
+				size={'md'}
+			/>
+			{/* </StackLayout> */}
 		</Form>
 	);
 };
--- src/pages/ideas/components/ideacard/IdeaCardFooter.tsx
+++ src/pages/ideas/components/ideacard/IdeaCardFooter.tsx
@@ -7,37 +7,39 @@ import React, { memo } from 'react';
 import { IoChatboxSharp } from 'react-icons/io5';
 import IdeaMenu from '../IdeaMenu';
 
-const IdeaCardFooter = (idea: TIdea_Preview): JSX.Element => (
-	<StackLayout
-		direction={'row'}
-		spacing={0}
-		rounded={'none'}
-		mb={2}
-		w={'full'}
-		justifyContent={'space-between'}
-		alignItems={'center'}
-		borderTopWidth={1}
-		borderTopColor={'gray.100'}
-	>
-		<StackLayout direction={'row'} spacing={2} alignItems={'center'}>
-			<IdeaUpvote {...idea} />
-			{idea?.totalComments > 0 && (
-				<FlexLayout alignItems={'center'}>
-					<Icon
-						as={IoChatboxSharp}
-						fontSize={'sm'}
-						pt={0}
-						mr={1}
-						color={'gray.400'}
-					/>
-					<Label color={'gray.400'} fontSize={'xs'}>
-						{idea.totalComments}
-					</Label>
-				</FlexLayout>
-			)}
+const IdeaCardFooter = (idea: TIdea_Preview): JSX.Element => {
+	return (
+		<StackLayout
+			direction={'row'}
+			spacing={0}
+			rounded={'none'}
+			mb={2}
+			w={'full'}
+			justifyContent={'space-between'}
+			alignItems={'center'}
+			borderTopWidth={1}
+			borderTopColor={'gray.100'}
+		>
+			<StackLayout direction={'row'} spacing={2} alignItems={'center'}>
+				<IdeaUpvote {...idea} />
+				{idea?.totalComments > 0 && (
+					<FlexLayout alignItems={'center'}>
+						<Icon
+							as={IoChatboxSharp}
+							fontSize={'sm'}
+							pt={0}
+							mr={1}
+							color={'gray.400'}
+						/>
+						<Label color={'gray.400'} fontSize={'xs'}>
+							{idea.totalComments}
+						</Label>
+					</FlexLayout>
+				)}
+			</StackLayout>
+			<IdeaMenu {...idea} />
 		</StackLayout>
-		<IdeaMenu {...idea} />
-	</StackLayout>
-);
+	);
+};
 
 export default memo(IdeaCardFooter);
