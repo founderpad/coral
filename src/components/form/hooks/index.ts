297ee9d0ed35595e847249d0aa6203751a863b41
Jamie <jamie@Jamies-MacBook-Air.local>
2022-01-06 14:28:57 +0000

    Trigger build to see if it works...

--- src/components/form/hooks/index.ts
+++ src/components/form/hooks/index.ts
@@ -1,11 +1,14 @@
-import { useForm, UseFormReturn } from "react-hook-form";
+import { UseFormReturn } from 'react-hook-form';
 
 /**
- * 
+ *
  * @returns form
  */
-export const useBaseForm = <T, U extends Record<string, unknown> = Record<string, unknown>>(): UseFormReturn<T, U> => {
-    const form = useForm<T>({ mode: 'all' });
+export const useBaseForm = <
+	T,
+	U extends Record<string, unknown> = Record<string, unknown>
+>(): UseFormReturn<T, U> => {
+	// const form = useForm<T>({ mode: 'all' });
 
-    return form
-}
+	return null;
+};
