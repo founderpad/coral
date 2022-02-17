004651867db88a102de5ea4e784c18bc436e84b9
Jamie <jamie@Jamies-MacBook-Air.local>
2022-02-17 16:50:04 +0000

    Updated meta tags

--- src/pages/idea/[id].tsx
+++ src/pages/idea/[id].tsx
@@ -4,22 +4,43 @@ import { useCurrentUser } from '@hooks/auth';
 import { useQueryParam } from '@hooks/util';
 import AuthFilter from '@utils/AuthFilter';
 import { NextPage } from 'next';
+import Head from 'next/head';
 import React from 'react';
 import ViewIdeaTabLayout from './ViewIdeaTabLayout';
 
 const ViewIdea: NextPage = () => {
 	const user = useCurrentUser();
-	// const [data, setData] = useState<TIdeaQuery>({});
+	const ideaId = useQueryParam('id');
 
 	useIdeaQuery({
 		variables: {
-			id: useQueryParam('id'),
+			id: ideaId,
 			userId: user?.id
 		}
 	});
 
 	return (
 		<React.Fragment>
+			<Head>
+				<meta
+					property="og:title"
+					content="Founderpad · View idea"
+					key="title"
+				/>
+				<meta
+					property="og:url"
+					content={`https://app.founderpad.com/idea/${ideaId}`}
+				/>
+				<meta
+					property="og:image"
+					content="https://founderpad-file-uploads.s3.eu-west-1.amazonaws.com/founderpad-og-image%40256x256.png"
+				/>
+				<meta property="og:site_name" content="Founderpad" />
+				<meta
+					property="og:description"
+					content="View this idea and see whether or not you would be interested in collaborating on it, and how popular it is with other users."
+				/>
+			</Head>
 			<DocumentTitle title="View idea" />
 			<ViewIdeaTabLayout />
 		</React.Fragment>
--- src/pages/ideas/index.tsx
+++ src/pages/ideas/index.tsx
@@ -2,6 +2,7 @@
 import SearchContentGridLayout from '@components/layouts/SearchContentGridLayout';
 import { DocumentTitle, PageHeader } from '@components/shared';
 import AuthFilter from '@utils/AuthFilter';
+import Head from 'next/head';
 import React from 'react';
 import IdeasSearchForm from './components/IdeasSearchForm';
 import IdeasContainer from './IdeasContainer';
@@ -18,6 +19,26 @@ const Ideas = () => (
 				<IdeasContainer />
 			</SearchContentGridLayout>
 		</PageLayout> */}
+		<Head>
+			<meta
+				property="og:title"
+				content="Founderpad · Search ideas"
+				key="title"
+			/>
+			<meta
+				property="og:url"
+				content="https://app.founderpad.com/ideas?page=1"
+			/>
+			<meta
+				property="og:image"
+				content="https://founderpad-file-uploads.s3.eu-west-1.amazonaws.com/founderpad-og-image%40256x256.png"
+			/>
+			<meta property="og:site_name" content="Founderpad" />
+			<meta
+				property="og:description"
+				content="Search all ideas to collaborate, or form a team"
+			/>
+		</Head>
 
 		<SearchContentGridLayout>
 			<IdeasSearchForm />
