b005922cc8d578f9401b06b3528be974c934756e
Jamie <jamie@Jamies-MacBook-Air.local>
2022-02-18 18:30:29 +0000

    Updated to include filters for most popular by upvotes and comments

--- src/pages/ideas/components/IdeasSearchForm.tsx
+++ src/pages/ideas/components/IdeasSearchForm.tsx
@@ -1,18 +1,26 @@
+import { Button, Checkbox, FormControl } from '@chakra-ui/react';
 import { SubmitButton } from '@components/buttons';
 import Form from '@components/form/Form';
 import BaseHeading from '@components/heading/BaseHeading';
 import { InputField } from '@components/input/InputField';
 import { SelectField } from '@components/input/SelectField';
+import { Label } from '@components/labels';
+import { FlexLayout } from '@components/layouts';
+import { AppDivider } from '@components/shared';
 import ModalDrawerContext from '@context/ModalDrawerContext';
 import {
+	ALL_COUNTRIES,
 	ALL_IDEA_CATEGORY_FIELDS,
 	ALL_IDEA_STATUSES,
+	BY_IDEA_POPULARITY,
+	mobileCountriesList,
 	mobileIdeaCategoryFields,
+	mobileIdeaPopularity,
 	mobileIdeaStatuses
 } from '@utils/Constants';
-import { useRouter } from 'next/router';
-import React, { useContext } from 'react';
-import { useForm } from 'react-hook-form';
+import Router, { useRouter } from 'next/router';
+import React, { useCallback, useContext, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 
 type SelectSearch = {
 	label: string;
@@ -22,7 +30,7 @@ type SelectSearch = {
 type IdeaSearch = {
 	field?: SelectSearch;
 	name?: string;
-	is_new?: boolean;
+	isNew?: boolean;
 	status?: SelectSearch;
 };
 
@@ -30,12 +38,14 @@ const IdeasSearchForm = () => {
 	const { setModalDrawer } = useContext(ModalDrawerContext);
 	const router = useRouter();
 	const { page, ...rest } = router.query;
+	const [isNewIdea, setIsNewIdea] = useState(false);
 
-	const { handleSubmit, control, reset, getValues } = useForm<IdeaSearch>({
-		defaultValues: {
-			...rest
-		}
-	});
+	const { handleSubmit, control, reset, getValues, setValue } =
+		useForm<IdeaSearch>({
+			defaultValues: {
+				...rest
+			}
+		});
 
 	const onClick = (values: IdeaSearch) => {
 		setModalDrawer({
@@ -57,6 +67,8 @@ const IdeasSearchForm = () => {
 		!queryParams.name && delete queryParams['name'];
 		!queryParams.field && delete queryParams['field'];
 		!queryParams.status && delete queryParams['status'];
+		!queryParams.country && delete queryParams['country'];
+		!queryParams.isNew && delete queryParams['is_new'];
 
 		router.push(
 			{
@@ -70,33 +82,33 @@ const IdeasSearchForm = () => {
 		);
 	};
 
-	// const onClear = () => {
-	// 	setModalDrawer({
-	// 		isOpen: false
-	// 	});
-	// 	reset({
-	// 		name: '',
-	// 		field: undefined,
-	// 		status: undefined
-	// 	});
-
-	// 	router.push(
-	// 		{
-	// 			pathname: '/ideas',
-	// 			query: { page: 1 }
-	// 		},
-	// 		undefined,
-	// 		{
-	// 			shallow: true
-	// 		}
-	// 	);
-	// };
-
 	const resetField = (name: string) => {
 		reset({
 			...getValues(),
 			[name]: ''
 		});
+	};
+
+	const onSetNewIdea = useCallback(() => {
+		setValue('isNew', !isNewIdea);
+		setIsNewIdea(!isNewIdea);
+	}, [isNewIdea]);
+
+	const onClearNewIdeas = () => {
+		delete Router.query['isNew'];
+		setValue('isNew', false);
+		setIsNewIdea(false);
+
+		Router.push(
+			{
+				pathname: Router.pathname,
+				query: Router.query
+			},
+			undefined,
+			{
+				shallow: true
+			}
+		);
 	};
 
 	return (
@@ -109,12 +121,23 @@ const IdeasSearchForm = () => {
 			<BaseHeading fontSize={'sm'} display={{ base: 'none', sm: 'flex' }}>
 				Filters
 			</BaseHeading>
+			<SelectField
+				id="country"
+				name="country"
+				label="Country"
+				placeholder={'country'}
+				options={ALL_COUNTRIES}
+				mobileOptions={mobileCountriesList()}
+				onClear={() => resetField('country')}
+				control={control}
+				isUrl
+			/>
 			<InputField
 				id="name"
 				name="name"
 				placeholder="Search name"
 				control={control}
-				label={'Name'}
+				label={'Idea name'}
 				variant={'filled'}
 				onClear={() => resetField('name')}
 				isUrl
@@ -143,6 +166,60 @@ const IdeasSearchForm = () => {
 				variant={'filled'}
 				isUrl
 			/>
+
+			<SelectField
+				id="popularity"
+				name="popularity"
+				options={BY_IDEA_POPULARITY}
+				mobileOptions={mobileIdeaPopularity()}
+				onClear={() => resetField('popularity')}
+				placeholder="most popular"
+				control={control}
+				label={'Most popular'}
+				variant={'filled'}
+				isUrl
+			/>
+
+			<AppDivider />
+
+			<FormControl>
+				<FlexLayout justifyContent={'space-between'}>
+					<Controller
+						name={'isNew'}
+						render={({ field: { ref } }) => (
+							<Checkbox
+								name={'isNew'}
+								rounded={'none'}
+								focusBorderColor={'gray.150'}
+								value={1}
+								py={1}
+								onChange={onSetNewIdea}
+								colorScheme={'fpPrimary'}
+								color={'fpGrey.900'}
+								ref={ref}
+								isChecked={isNewIdea}
+							>
+								<Label
+									color={'fpGrey.900'}
+									fontSize={{ base: 'small', sm: 'xs' }}
+								>
+									New ideas
+								</Label>
+							</Checkbox>
+						)}
+						control={control}
+					/>
+					<Button
+						fontSize={'x-small'}
+						colorScheme={'fpPrimary'}
+						variant={'link'}
+						mb={1}
+						onClick={onClearNewIdeas}
+					>
+						Clear
+					</Button>
+				</FlexLayout>
+			</FormControl>
 
 			<SubmitButton
 				name={'filter-ideas-button'}
