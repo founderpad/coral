0f29302aee019a9f2562a56bddc245505694ad27
Jamie <jamie@Jamies-MacBook-Air.local>
2022-02-24 17:23:12 +0000

    Updated to make search queries more robust and typed.

---
+++ src/utils/routerUtils.ts
@@ -0,0 +1,59 @@
+import Router from 'next/router';
+
+type TParam = {
+	[key: number]: string | string[] | boolean;
+};
+
+/**
+ * Toggle whether to do a router push or replace
+ * @param pathname
+ * @param query
+ * @param replace
+ * @param shallow
+ */
+const navigateTo = (
+	pathname?: string,
+	query?: any,
+	replace = false,
+	shallow = true
+) => {
+	Router[replace ? 'replace' : 'push'](
+		{
+			pathname,
+			query: { ...Router.query, ...query }
+		},
+		undefined,
+		{
+			shallow
+		}
+	);
+};
+
+/** Remove a single param */
+const deleteParam = <T extends TParam>(key: keyof T & string) => {
+	if (Router.query[key]) {
+		delete Router['query'][key];
+		navigateTo();
+	}
+};
+
+/** Remove multiple params */
+const deleteParams = <T extends TParam>(values: T) => {
+	for (const [key, value] of Object.entries(values)) {
+		if (!value) delete Router['query'][key];
+	}
+
+	navigateTo();
+};
+
+/** Build parameters for the URL */
+const buildParams = <T extends TParam>(values: T) => {
+	const queryParams = JSON.parse(JSON.stringify(values));
+	for (const [key, value] of Object.entries(queryParams)) {
+		if (!value) delete queryParams[key];
+	}
+
+	navigateTo(undefined, { ...queryParams });
+};
+
+export { navigateTo, deleteParam, deleteParams, buildParams };
