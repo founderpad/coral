c50491bdb1ef728653561be52357b3c690cdc5bc
Jamie <jamie@Jamies-MacBook-Air.local>
2022-03-16 16:51:00 +0000

    Updated to see if emails send from specific email

--- functions/email/report/send-reported-email.js
+++ functions/email/report/send-reported-email.js
@@ -12,11 +12,11 @@ const AWS_SES = new AWS.SES(CONFIG);
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default (req, res) => {
 	AWS_SES.sendEmail({
-		Source: 'support@founderpad.com',
+		Source: 'jamie@founderpad.com',
 		Destination: {
 			ToAddresses: [`${req.body.event.data.new.recipient_email}`],
 			// ToAddresses: ['success@simulator.amazonses.com'],
-			BccAddresses: ['jamie@founderpad.com', 'toby@founderpad.com']
+			BccAddresses: ['jamie@founderpad.com']
 		},
 		Message: {
 			Body: {
@@ -25,19 +25,7 @@ export default (req, res) => {
 					Data: `<html>
 	                            <head></head>
 	                            <body>
-	                                <p>Hi ${
-										req.body.event.data.new.recipient_name
-									},</p>
-	                                <p>Your ${req.body.event.data.new.type.toLowerCase()} <i>
-										"${
-											req.body.event.data.new.content
-										}"</i> has been reported for the following reason:
-	                                    <strong>${
-											req.body.event.data.new.reason
-										}</strong>
-	                                </p>
-	                                <p>We will carefully monitor this and, if necessary, will remove this ${req.body.event.data.new.type.toLowerCase()}.</p><br/>
-	                                <strong>The founderpad team</strong>
+	                                <p>Hi ${req.body.event.data.new.recipient_name},</p>
 	                            </body>
 	                        </html>`
 				}
