f3f3aea360f9ef14d717ff2214b8e73caeca7e1b
Jamie <jamie@Jamies-MacBook-Air.local>
2022-03-31 19:05:15 +0100

    Updated to use onesignal-node library for more flexibility. Let's see if this works

--- functions/notifications/send-push-interested-idea.js
+++ functions/notifications/send-push-interested-idea.js
@@ -1,35 +1,11 @@
-var sendNotification = async function (message) {
-	var headers = {
-		'Content-Type': 'application/json; charset=utf-8',
-		Authorization: 'Basic Yjg0ODY2NmQtMjZmYS00ZmRiLWIzMTUtOWY1ZGJiOTc2YjY1'
-	};
+const OneSignal = require('onesignal-node');
 
-	var options = {
-		host: 'onesignal.com',
-		port: 443,
-		path: '/api/v1/notifications',
-		method: 'POST',
-		headers: headers
-	};
+const client = new OneSignal.Client(
+	'a890c1f8-d682-4225-ae11-01f7cd717b84',
+	'Yjg0ODY2NmQtMjZmYS00ZmRiLWIzMTUtOWY1ZGJiOTc2YjY1'
+);
 
-	var https = require('https');
-	var req = https.request(options, function (res) {
-		res.on('data', function (data) {
-			console.log('Response:');
-			console.log(JSON.parse(data));
-		});
-	});
-
-	req.on('error', function (e) {
-		console.log('ERROR:');
-		console.log(e);
-	});
-
-	req.write(JSON.stringify(message));
-	req.end();
-};
-
-export default (req, res) => {
+export default async (req, res) => {
 	const fromUserId = req.body.event.data.new.user_id;
 	const targetUserId = req.body.event.data.new.target_user_id;
 	const ideaId = req.body.event.data.new.idea_id;
@@ -43,12 +19,70 @@ export default (req, res) => {
 			en: 'Somebody is interested in your idea! 🚀   Click here to see who they are'
 		},
 		url: `https://app.founderpad.com/idea/${ideaId}`,
+		channel_for_external_user_ids: 'push',
 		include_external_user_ids: [targetUserId]
 	};
 
-	sendNotification(message);
-	// if (response)
-	// 	res.status(200).send(
-	// 		`nterested idea notification sent successfully to ${req.body.event.data.new.target_user_id}`
-	// 	);
+	try {
+		const response = await client.createNotification(notification);
+		// console.log(response.body.id);
+	} catch (e) {
+		if (e instanceof OneSignal.HTTPError) {
+			// When status code of HTTP response is not 2xx, HTTPError is thrown.
+			console.log(e.statusCode);
+			console.log(e.body);
+		}
+	}
 };
+
+// var sendNotification = async function (message) {
+// 	var headers = {
+// 		'Content-Type': 'application/json; charset=utf-8',
+// 		Authorization: 'Basic Yjg0ODY2NmQtMjZmYS00ZmRiLWIzMTUtOWY1ZGJiOTc2YjY1'
+// 	};
+
+// 	var options = {
+// 		host: 'onesignal.com',
+// 		port: 443,
+// 		path: '/api/v1/notifications',
+// 		method: 'POST',
+// 		headers: headers
+// 	};
+
+// 	var https = require('https');
+// 	var req = https.request(options, function (res) {
+// 		res.on('data', function (data) {
+// 			console.log('Response:');
+// 			console.log(JSON.parse(data));
+// 		});
+// 	});
+
+// 	req.on('error', function (e) {
+// 		console.log('ERROR:');
+// 		console.log(e);
+// 	});
+
+// 	req.write(JSON.stringify(message));
+// 	req.end();
+// };
+
+// export default (req, res) => {
+// 	const fromUserId = req.body.event.data.new.user_id;
+// 	const targetUserId = req.body.event.data.new.target_user_id;
+// 	const ideaId = req.body.event.data.new.idea_id;
+
+// 	if (fromUserId === targetUserId) return null;
+
+// 	const message = {
+// 		app_id: 'a890c1f8-d682-4225-ae11-01f7cd717b84',
+// 		en: 'text',
+// 		contents: {
+// 			en: 'Somebody is interested in your idea! 🚀   Click here to see who they are'
+// 		},
+// 		url: `https://app.founderpad.com/idea/${ideaId}`,
+// 		channel_for_external_user_ids: 'push',
+// 		include_external_user_ids: [targetUserId]
+// 	};
+
+// 	sendNotification(message);
+// };
