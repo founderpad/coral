a6c62bdc15a808a8e6be6dceb17b4f3b6f89e2fc
Jamie <jamie@Jamies-MacBook-Air.local>
2022-04-19 20:24:24 +0100

    Testing...

--- src/hooks/notifications.ts
+++ src/hooks/notifications.ts
@@ -7,6 +7,8 @@ declare let window: OneSignalWindow;
 export const usePushNotifications = () => {
 	const userId = useAuth().user?.id;
 
+	console.log('auth: ', useAuth());
+
 	useEffect(() => {
 		window.OneSignal = window.OneSignal || [];
 		if (window.OneSignal) {
