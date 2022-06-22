5185f7217eac916b30e65c270242c8b56b19e255
Jamie <jamie@Jamies-MacBook-Air.local>
2022-06-22 09:52:41 +0100

    Updated

--- src/pages/ideas/boost/BoostedIdeasGridLayout.tsx
+++ src/pages/ideas/boost/BoostedIdeasGridLayout.tsx
@@ -1,31 +1,34 @@
-import { GridItem, SimpleGrid } from '@chakra-ui/react';
+import { NoResults } from '@/components/shared';
+// import { GridItem, SimpleGrid } from '@chakra-ui/react';
 import React from 'react';
 
 const BoostedIdeasGridLayout = () => {
-	const arr = [...Array(20).keys()];
-	return (
-		<SimpleGrid
-			columns={{ base: 1, md: 2 }}
-			minH="full"
-			rowGap={6}
-			columnGap={6}
-			flex={1}
-			position="relative"
-			bg="white"
-			p={{ base: 4, md: 0 }}
-		>
-			{arr.map((a) => (
-				<GridItem
-					key={a}
-					borderWidth={1}
-					p={4}
-					rounded={{ base: 'none', lg: 'md' }}
-				>
-					{a}
-				</GridItem>
-			))}
-		</SimpleGrid>
-	);
+	// const arr = [...Array(20).keys()];
+
+	return <NoResults />;
+	// return (
+	// 	<SimpleGrid
+	// 		columns={{ base: 1, md: 2 }}
+	// 		minH="full"
+	// 		rowGap={6}
+	// 		columnGap={6}
+	// 		flex={1}
+	// 		position="relative"
+	// 		bg="white"
+	// 		p={{ base: 4, md: 0 }}
+	// 	>
+	// 		{arr.map((a) => (
+	// 			<GridItem
+	// 				key={a}
+	// 				borderWidth={1}
+	// 				p={4}
+	// 				rounded={{ base: 'none', lg: 'md' }}
+	// 			>
+	// 				{a}
+	// 			</GridItem>
+	// 		))}
+	// 	</SimpleGrid>
+	// );
 };
 
 export default BoostedIdeasGridLayout;
