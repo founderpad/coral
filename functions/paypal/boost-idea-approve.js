2b6bcf521d211572bcf70bdf9ad5190dc81f0984
Jamie <jamie@Jamies-MacBook-Air.local>
2022-08-16 17:09:30 +0100

    WUpdated to insert boosted idea...

--- functions/paypal/boost-idea-approve.js
+++ functions/paypal/boost-idea-approve.js
@@ -12,26 +12,22 @@ const INSERT_BOOSTED_IDEA = gql`
 
 export default async (req, res) => {
 	// isValidSecret(req);
-	// const ideaId = req.body.event.data.new.custom_id;
-	// const ideaId = '3cb8f33e-af2b-4325-a452-0a830fcc4e30';
 	const ideaId = res?.req?.body?.resource?.custom_id;
-
 	// if (!userId) throw 'No user id found';
 
-	// const ideaId = req.body.event
+	if (ideaId) {
+		try {
+			const response = await graphqlClient.request(INSERT_BOOSTED_IDEA, {
+				ideaId
+			});
+			res.status(200).send(`Idea with id ${ideaId} added to boost`);
+		} catch (error) {
+			// throw `Failed to insert esteem points for user with id: ${userId}`;
+			res.status(500).send(
+				error.message + ' --- Failed to create esteem points for user'
+			);
+		}
+	}
 
-	console.log('idea id log: ', ideaId);
-	res.status(200).send(ideaId);
-
-	// try {
-	// 	const response = await graphqlClient.request(INSERT_BOOSTED_IDEA, {
-	// 		ideaId
-	// 	});
-	// 	res.status(200).send(`Idea with id ${ideaId} added to boost`);
-	// } catch (error) {
-	// 	// throw `Failed to insert esteem points for user with id: ${userId}`;
-	// 	res.status(500).send(
-	// 		error.message + ' --- Failed to create esteem points for user'
-	// 	);
-	// }
+	return null;
 };
