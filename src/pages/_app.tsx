58b7e6b356167cc5d57c84f0546bff5ee31fe532
Jamie <jamie@Jamies-MacBook-Air.local>
2023-06-01 12:19:53 +0100

    Update env vars

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,7 +30,7 @@ const persistor = persistStore(store);
 
 const nhost = new NhostClient({
 	subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN,
-	region: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN
+	region: process.env.NEXT_PUBLIC_NHOST_REGION
 });
 
 const auth = nhost.auth;
