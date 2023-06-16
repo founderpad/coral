6bdd7a9492cfc2ca44a4672fc8fe32584a16000f
Jamie <jamie@Jamies-MacBook-Air.local>
2023-06-16 10:21:24 +0100

    Update to link to different pages in the home tab

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 import useChangePasswordModal from '../account/profile/hooks/useChangePasswordModal';
 import { List, ListItem, OrderedList, Text } from '@chakra-ui/react';
+import { BaseLink } from '@/components/links';
 
 const Home: NextPage = () => {
 	const user = useUserData();
@@ -37,18 +38,30 @@ const Home: NextPage = () => {
 					</Text>
 					<OrderedList>
 						<ListItem fontSize="sm">
-							Click the top right profile section and select
-							&quot;Profile&quot;.
+							Click the top right profile section and select{' '}
+							<Text as={BaseLink} href="/account/profile">
+								Profile
+							</Text>
+							.
 						</ListItem>
 						<ListItem fontSize="sm">
-							In the &quot;Details&quot; tab, you can go through
-							and update all fields that you would like to display
-							in your profile.
+							In the{' '}
+							<Text as={BaseLink} href="/account/profile">
+								Details
+							</Text>{' '}
+							tab, you can go through and update all fields that
+							you would like to display in your profile.
 						</ListItem>
 						<ListItem fontSize="sm">
-							If you would like to use our &quot;Match&quot;
-							feature, then select the &quot;Match&quot; tab and
-							fill in all fields.
+							If you would like to use our{' '}
+							<Text as={BaseLink} href="/match">
+								Match
+							</Text>{' '}
+							feature, then select the{' '}
+							<Text as={BaseLink} href="/match">
+								Match
+							</Text>{' '}
+							tab and fill in all fields.
 						</ListItem>
 						<ListItem fontSize="sm">
 							Once your profile is setup, you will find it easier
@@ -64,24 +77,39 @@ const Home: NextPage = () => {
 					</Text>
 					<OrderedList>
 						<ListItem fontSize="sm">
-							Navigate to ideas by hovering over &quot;Ideas&quot;
-							and selecting &quot;Search Ideas&quot;. This will
-							display all startup ideas, which can be filtered to
-							refine your search.
+							Navigate to ideas by hovering over{' '}
+							<Text as={BaseLink} href="/ideas/search?page=1">
+								Ideas
+							</Text>{' '}
+							and selecting{' '}
+							<Text as={BaseLink} href="/ideas/search?page=1">
+								Search Ideas
+							</Text>
+							. This will display all startup ideas, which can be
+							filtered to refine your search.
 						</ListItem>
 						<ListItem fontSize="sm">
-							To post a new idea, click the blue &quot;+ New
-							Idea&quot; button at the top, fill out all required
-							fields and include all the information you would
-							like, and then press &quot;Create Your Idea&quot;,
-							and this will publish it.
+							To post a new idea, click the blue{' '}
+							<Text as={BaseLink} href="/ideas/create">
+								+ New Idea
+							</Text>{' '}
+							button at the top, fill out all required fields and
+							include all the information you would like, and then
+							press &quot;Create Your Idea&quot;, and this will
+							publish it.
 						</ListItem>
 						<ListItem fontSize="sm">
-							To see your published idea, hover over
-							&quot;Ideas&quot;, and select &quot;My Ideas&quot;.
-							This will display any published or unpublished ideas
-							you have created, and this is where you can edit
-							them.
+							To see your published idea, hover over{' '}
+							<Text as={BaseLink} href="/ideas/myideas">
+								Ideas
+							</Text>
+							, and select{' '}
+							<Text as={BaseLink} href="/ideas/myideas">
+								My Ideas
+							</Text>
+							. This will display any published or unpublished
+							ideas you have created, and this is where you can
+							edit them.
 						</ListItem>
 					</OrderedList>
 				</BoxLayout>
@@ -92,9 +120,16 @@ const Home: NextPage = () => {
 					</Text>
 					<OrderedList>
 						<ListItem fontSize="sm">
-							In the top panel, hover over &quot;Users&quot;, and
-							select &quot;Search Users&quot;. This is where you
-							can see all of the users on the platform.
+							In the top panel, hover over{' '}
+							<Text as={BaseLink} href="/users/search?page=1">
+								Users
+							</Text>
+							, and select{' '}
+							<Text as={BaseLink} href="/users/search?page=1">
+								Search Users
+							</Text>
+							. This is where you can see all of the users on the
+							platform.
 						</ListItem>
 						<ListItem fontSize="sm">
 							To filter down the search to find specific users in
@@ -111,10 +146,10 @@ const Home: NextPage = () => {
 					</Text>
 					<List>
 						<ListItem fontSize="sm">
-							To filter down the search to find specific users in
-							your field of interest, use the filters on the left
-							hand panel and select &quot;Show Results&quot; at
-							the bottom of the panel.
+							Match will only work if you fill out the Match
+							details tab in your profile section, and this will
+							show your closest matches to what you are looking
+							for on Founderpad.
 						</ListItem>
 					</List>
 				</BoxLayout>
@@ -125,8 +160,11 @@ const Home: NextPage = () => {
 					</Text>
 					<List>
 						<ListItem fontSize="sm">
-							In the top panel, select &quot;Messages&quot; and
-							this will show you any messages you have sent or
+							In the top panel, select{' '}
+							<Text as={BaseLink} href="/messages">
+								Messages
+							</Text>{' '}
+							and this will show you any messages you have sent or
 							received from other users on the platform.
 						</ListItem>
 					</List>
