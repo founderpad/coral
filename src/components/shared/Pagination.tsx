4bfbbc670c774c5f2639d598d02ca33f4955b9ad
Jamie <jamie@Jamies-MacBook-Air.local>
2023-06-21 13:37:17 +0100

    Update to fix issue with pagination scrolling issue.

--- src/components/shared/Pagination.tsx
+++ src/components/shared/Pagination.tsx
@@ -35,8 +35,7 @@ export const Pagination = (props: Props) => {
 			/>
 			<ButtonGroup
 				display={{ base: 'none', sm: 'flex' }}
-				flex={1}
-				justifyContent="center"
+				alignSelf="center"
 				overflowX="auto"
 			>
 				{pagesArray.map((p) => (
